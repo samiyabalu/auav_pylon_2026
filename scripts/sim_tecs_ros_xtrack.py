92bbecb0dc43dbdd9d70302b7d7ce5e3ac942ab0
sammy <samiya.balu@gmail.com>
2025-12-04 10:03:42 -0500

    added more intermediate wpts

--- scripts/sim_tecs_ros_xtrack.py
+++ scripts/sim_tecs_ros_xtrack.py
@@ -23,13 +23,14 @@ def wrap(x):
 # ## SIM
 alt = 7.0
 control_point = [
-(-20.0, -17.5, alt),
-(-15.4, -25.5, alt),
+(-22.0, -17.5, alt),
+(-25.4, -22.5, alt),
+(-5.4, -25.5, alt),
 (20.5,-28.4, alt),
 (23.7,-15.1, alt),
 (10.5, -2.4, alt),
-# (-8.4, -8, alt),
-(-20.0, -17.5, alt),
+(-8.4, -8, alt),
+(-22.0, -17.5, alt),
 ]
 
 # Get coordinates for reference line
